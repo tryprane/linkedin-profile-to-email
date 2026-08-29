d70e1a5697a9f23c1cde20665edb45745ba0e5d6
tryprane <pranjalyadavtricks@gmail.com>
2026-08-29 22:07:34 +0530

    perf: set 1024MB lean RAM and Chromium performance flags

--- src/solver.py
+++ src/solver.py
@@ -78,6 +78,16 @@ class TurnstileSolver:
                 '--disk-cache-size=104857600',
                 '--ignore-certificate-errors',
                 '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--disable-software-rasterizer',
+                '--disable-background-networking',
+                '--disable-default-apps',
+                '--disable-sync',
+                '--no-first-run',
+                '--mute-audio',
+                '--blink-settings=imagesEnabled=false',
             ]
 
             fetch_kwargs = {
